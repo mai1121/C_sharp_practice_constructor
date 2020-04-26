2471ed349dafbb879fe9e0723b76837a0de42dc5
mai1121 <maimaiukawa@gmail.com>
2020-04-26 21:39:09 +0900

    class_field,method,const,static_constructor

--- practice_constructor/Program.cs
+++ practice_constructor/Program.cs
@@ -20,10 +20,23 @@ namespace practice_constructor
                 author = "三島由紀夫",
                 title = "春の雪"
             };
+
+
             Console.WriteLine(b3.Show());
+            //クラス定数呼び出し
+            Console.WriteLine($"１科目めは{Score.Subject1}です");
+            Console.WriteLine($"２科目めは{Score.Subject2}です");
+            //クラスメソッド呼び出し
+            Score.TotalScore(70,40);
+            //クラスフィールド呼び出し
+            Console.WriteLine($"合格点は{Score.PassingScore}点です");
+
+            var s1 = new Score(90,67);
+            var s2 = new Score(86,78);
         }
     }
 
+    #region Booksクラス定義
     class Books
     {
         public string title;
@@ -48,6 +61,38 @@ namespace practice_constructor
 
 
     }
+    #endregion
 
- 
+    class Score
+    {
+        public int japanese_language;
+        public int sociology;
+
+        //静的コンストラクター
+        static Score()
+        {
+            Console.WriteLine("点数発表");
+        }
+
+        //クラスフィールド
+        public static int PassingScore = 120;
+
+        //クラス定数
+        public static readonly string Subject1 = "数学";
+        public const string Subject2 = "化学";
+
+        //クラスメソッド
+        public static void TotalScore(int math,int science)
+        {
+            Console.WriteLine($"合計点は{math+science}です");
+        }
+
+        //通常のコンストラクター
+        public Score(int japanese_language, int sociology)
+        {
+            this.japanese_language = japanese_language;
+            this.sociology = sociology;
+            Console.WriteLine($"国語は{japanese_language}点で社会は{sociology}点です");
+        }
+    }
 }
