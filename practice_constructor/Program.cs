f198e2fa035ec68d44f2870a17734cfe8219c00a
mai1121 <maimaiukawa@gmail.com>
2020-04-27 22:15:40 +0900

    default_parameter

--- practice_constructor/Program.cs
+++ practice_constructor/Program.cs
@@ -33,6 +33,20 @@ namespace practice_constructor
 
             var s1 = new Score(90,67);
             var s2 = new Score(86,78);
+
+            var l1 = new Lunch() { 
+            mainDish="カレー",
+            subDish="切り干し大根"
+            };
+            //既定値をそのまま使用して呼び出し
+            l1.Show();
+            //既定値を上書き
+            l1.Show("飲むヨーグルト");
+            //順番入れ替えながら既定値を上書き
+            l1.Show(dessert:"いちご",drink:"お茶");
+            //引数に何個指定してもOK
+            Console.WriteLine($"総カロリーは{l1.Calory(200, 300, 450, 20)}です");
+            Console.WriteLine(l1.CaloryOld("5年生",900,30,20,300));
         }
     }
 
@@ -63,6 +77,7 @@ namespace practice_constructor
     }
     #endregion
 
+    #region Scoreクラス定義
     class Score
     {
         public int japanese_language;
@@ -95,4 +110,40 @@ namespace practice_constructor
             Console.WriteLine($"国語は{japanese_language}点で社会は{sociology}点です");
         }
     }
+    #endregion
+
+    #region
+    class Lunch
+    {
+        public string mainDish;
+        public string subDish;
+
+        //既定値を設定する。
+        public void Show(string drink="牛乳",string dessert="みかん")
+        {
+            Console.WriteLine($"今日の献立は{drink}と{this.mainDish}と{this.subDish}と{dessert}です");
+        }
+        //可変長引数のメソッド
+        public int Calory(params int[] values)
+        {
+            int totalCalory = 0;
+            foreach(var i in values)
+            {
+                totalCalory += i;
+            }
+            return totalCalory;
+        }
+        //可変長引数は引数の最後に指定する
+        public string CaloryOld(string schoolYear,params int[] values)
+        {
+            int totalCalory = 0;
+            foreach (var i in values)
+            {
+                totalCalory += i;
+            }
+            return $"{schoolYear}の摂取カロリーは{totalCalory}です";
+        }
+
+    }
+    #endregion
 }
